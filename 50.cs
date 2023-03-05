d2f9857b243bea53c7406e37ec8a3beee26842bf
Семён Курочкин <sema.kurochkin@mail.ru>
2023-03-05 04:01:30 +0300

    Задача 50

---
+++ 50.cs
@@ -0,0 +1,11 @@
+// using static Shares1;
+
+// int row = Input("Количество строк ");
+// int columns = Input("Количество столбцов ");
+// double [,] table = Matrix(row, columns);
+// FillMatrixRandom(table);
+
+// int posicionRow = Input("Введите номер строки ");
+// int posicionColumn = Input("Введите номер столбца ");
+// Console.WriteLine();
+// Posicion(table, posicionRow, posicionColumn);
