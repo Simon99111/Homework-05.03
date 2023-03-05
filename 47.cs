b3bd91700b4595e66bca18b1c5db487139576460
Семён Курочкин <sema.kurochkin@mail.ru>
2023-03-05 04:01:08 +0300

    Задача 47

---
+++ 47.cs
@@ -0,0 +1,7 @@
+﻿// using static Shares1;
+
+// int row = Input("Количество строк ");
+// int columns = Input("Количество столбцов ");
+// double [,] table = Matrix(row, columns);
+// FillMatrixRandom(table);
+
